3129f313f83b3d916ecafaef93a4ae03390bc164
Jirho-BE <jirhoenciso1204@gmail.com>
2026-09-09 02:36:05 +0800

    Added a login and register page for SPA

--- static/app.js
+++ static/app.js
@@ -4,9 +4,13 @@ createApp({
   delimiters: ["[[", "]]"],
   setup() {
     const text = ref("Hello world");
+    const reg_or_log = ref(true); //true for login false for register
+    const authenticated = ref(false); //false by default to make the user login or register an account
 
     return {
       text,
+      reg_or_log,
+      authenticated,
     };
   },
 }).mount("#app");
